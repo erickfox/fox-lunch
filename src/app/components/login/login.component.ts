4a7deeb8dcb95499fde6e92194d819b803d6b177
Erick Pac <erick.pac@fox.com>
2018-08-13 16:14:47 -0600

    added login service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { User } from '../../services'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
+  model: any = {};
+
   constructor() {
-    
-  }  
+
+  }
+
+  login() {
+    const response = User.auth(this.model)
+    console.log(response)
+  }
 }
