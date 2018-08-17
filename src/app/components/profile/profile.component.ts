1aa18132059f994fa23bfd994b82509224e3126f
Erick Pac <erick.pac@fox.com>
2018-08-16 22:51:08 -0600

    set profile user information

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { User } from '../../models'
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent {
+  currentUser: User
 
+  constructor() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 }
